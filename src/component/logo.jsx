24d15267121572f00fc1327f4469c5e367882d31
buildbysachin <buildbysachin@gmail.com>
2026-06-28 18:01:36 +0530

    5th commit

--- src/component/logo.jsx
+++ src/component/logo.jsx
@@ -4,7 +4,7 @@ const Logo = () => {
   return (
     <div>
     <div className='flex justify-center py-4 '>
-        <img className='rounded-full w-36 h-36 border-2 border-b-emerald-950' src="src\assets\Zyloslogo.webp" alt="" />
+        <img className='rounded-full w-36 h-36 border-2 border-b-emerald-950' src="https://raw.githubusercontent.com/zylos-ai/.github/master/assets/logo.png" alt="" />
     </div>
     <div>
         <h1 className='text-4xl flex justify-center w-auto text-[#432818]'><i>Welcome on my website</i></h1>
