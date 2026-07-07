8b5fd3e7e9445d887329c6839b2b43c376d4eef6
buildbysachin <buildbysachin@gmail.com>
2026-07-07 21:14:23 +0530

    some changes

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className=' w-full bg-[#99582A] text-[#FFE6A7] flex justify-center p-2'>
+        <div className=' w-full bg-[#2c0330] text-[#ffffff] flex justify-center p-2'>
             <p>Made with ❤️ by Sachin Mogra</p>
         </div>
     )
